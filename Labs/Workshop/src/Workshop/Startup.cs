0459098c7046344fa303e0b2821f401f2b6b1950
Eirik Årdal <eaardal@outlook.com>
2016-01-11 11:13:53 +0100

    Added Getting Started lab files

---
+++ Labs/Workshop/src/Workshop/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Workshop
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+        
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseIISPlatformHandler();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+        
+        public static void Main(string[] args)
+        {
+            var app = new WebApplicationBuilder()
+                .UseServer("Microsoft.AspNet.Server.Kestrel")
+                .UseConfiguration(WebApplicationConfiguration.GetDefault(args))
+                .UseStartup<Startup>()
+                .Build();
+
+            app.Run();
+        }
+    }
+}
