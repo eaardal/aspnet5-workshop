6131c965d56061f5a46b990ebeb2e2a0f4e3e60b
Eirik Årdal <eaardal@outlook.com>
2016-01-12 11:49:40 +0100

    Tests

--- Labs/Workshop/test/Workshop.Tests/SomeTests.cs
+++ Labs/Workshop/test/Workshop.Tests/SomeTests.cs
@@ -2,10 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNet.Hosting.Internal;
+using Microsoft.Extensions.PlatformAbstractions;
+using Workshop.Console;
 using Xunit;
 
 namespace Workshop.Tests
 {
+    public class StartupTests
+    {
+        [Fact]
+        public void AddsConfigJsonToConfiguration()
+        {
+            //var appEnv = new DefaultApplicationEnvironment();
+            //var hostingEnv = new HostingEnvironment();
+            //var sut = new Workshop.Console.Startup(appEnv, hostingEnv);
+            
+        }
+    }
+
     public class SomeTests
     {
         [Fact]
